e1e668662441bbd0c8b5ad07430f0678268f4e7e
Agustin978 <loboagus10@gmail.com>
2023-05-17 11:06:54 -0300

    Agregados los componentes item para agregarlos en el listado de tareas y agregado un formulario para ingresar las tareas

---
+++ Componentes/ItemTarea.jsx
@@ -0,0 +1,11 @@
+import { ListGroup } from "react-bootstrap";
+
+const ItemTarea = () => {
+    return (
+        <div>
+            <ListGroup.Item>Un elemento agregado</ListGroup.Item>
+        </div>
+    );
+};
+
+export default ItemTarea;
