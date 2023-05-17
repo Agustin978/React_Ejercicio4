f67c7a2aa3b4e223e6bec0eb78439e54e4120158
Agustin978 <loboagus10@gmail.com>
2023-05-17 10:40:53 -0300

    Creado el maquetado para la pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import {Container} from 'react-bootstrap'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <Container className='my-5 mainPage'>
+      <h1 className='display-4 text-center'>Listado de Tareas</h1>
+      <hr />
+    </Container>
+    {/*aqui se agrega el componenete*/}
+    <footer className='bg-dark text-center text-secondary py-4'>
+      <p className='display-6'>&copy; Todos los derechos reservados</p>
+    </footer>
+    </>
   )
 }
 
